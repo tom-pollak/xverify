e1fbf0d47842997e0d9c005463ccd58c75b18ab6
Tom Pollak <tompollak1000@gmail.com>
2025-03-12 22:06:35 +0000

    grpo training

---
+++ src/xverify/trainers/grpo.py
@@ -0,0 +1,284 @@
+from typing import Callable, Optional, Union, Any, List
+
+from accelerate.utils import broadcast_object_list, gather, gather_object
+from datasets import Dataset, IterableDataset
+import torch
+from torch import nn
+from transformers import (
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+    TrainerCallback,
+    is_wandb_available,
+    Trainer,
+)
+from transformers.utils import is_peft_available
+from trl import GRPOTrainer, GRPOConfig
+from trl.data_utils import apply_chat_template, maybe_apply_chat_template
+from trl.import_utils import is_rich_available
+from trl.trainer.utils import pad
+
+from ..env import Environment
+from ..utils.logging_utils import print_prompt_completions_sample
+
+if is_peft_available():
+    from peft import PeftConfig  # type: ignore
+
+if is_wandb_available():
+    import wandb
+
+RewardFunc = Union[str, PreTrainedModel, Callable[[list, list], list[float]]]
+
+
+class GRPOEnvTrainer(GRPOTrainer):
+    def __init__(
+        self,
+        model: Union[str, PreTrainedModel],
+        env: Environment,
+        reward_funcs: Union[RewardFunc, list[RewardFunc]],
+        args: Optional[GRPOConfig] = None,
+        train_dataset: Optional[Union[Dataset, IterableDataset]] = None,
+        eval_dataset: Optional[Union[Dataset, IterableDataset]] = None,
+        processing_class: Optional[PreTrainedTokenizerBase] = None,
+        callbacks: Optional[list[TrainerCallback]] = None,
+        optimizers: tuple[
+            Optional[torch.optim.Optimizer], Optional[torch.optim.lr_scheduler.LambdaLR]
+        ] = (None, None),
+        peft_config: Optional["PeftConfig"] = None,
+        **kwargs,
+    ):
+        if not args.use_vllm:  # type: ignore
+            raise ValueError("vLLM must be enabled for GRPOEnvTrainer")
+        if not (
+            callable(reward_funcs)
+            or (
+                isinstance(reward_funcs, list)
+                and all(callable(f) for f in reward_funcs)
+            )
+        ):
+            raise ValueError(
+                "reward_funcs must be a function or a list of functions. Use vLLM to host neural reward models."
+            )
+        super().__init__(
+            model=model,
+            reward_funcs=reward_funcs,
+            args=args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            processing_class=processing_class,
+            callbacks=callbacks,
+            optimizers=optimizers,
+            peft_config=peft_config,
+            **kwargs,
+        )
+        self.env = env
+
+    def _generate_and_score_completions(
+        self, inputs: dict[str, Union[torch.Tensor, Any]]
+    ) -> dict[str, Union[torch.Tensor, Any]]:
+        device = self.accelerator.device
+        prompts = [x["prompt"] for x in inputs]  # type: ignore
+        prompts_text = [
+            maybe_apply_chat_template(example, self.processing_class)["prompt"]
+            for example in inputs
+        ]  # type: ignore
+        prompt_inputs = self.processing_class(
+            prompts_text,
+            return_tensors="pt",
+            padding=True,
+            padding_side="left",
+            add_special_tokens=False,  # type: ignore
+        )  # type: ignore
+        prompt_inputs = Trainer._prepare_inputs(self, prompt_inputs)  # type: ignore
+        prompt_ids, prompt_mask = (
+            prompt_inputs["input_ids"],
+            prompt_inputs["attention_mask"],
+        )
+
+        if self.max_prompt_length is not None:
+            prompt_ids = prompt_ids[:, -self.max_prompt_length :]
+            prompt_mask = prompt_mask[:, -self.max_prompt_length :]
+
+        if self.state.global_step != self._last_loaded_step:
+            self._move_model_to_vllm()
+            self._last_loaded_step = self.state.global_step
+
+        # Gather the original prompts in message dict form, not the text form
+        all_prompts = gather_object(prompts)
+        if self.accelerator.is_main_process:
+            env_result = self.env.generate(
+                prompts=all_prompts,
+                llm=self.llm,
+                sampling_params=self.sampling_params,
+            )
+            completion_ids = env_result["ids"]
+            completion_messages = env_result["messages"]
+            completion_mask = env_result["mask"]
+
+        else:
+            completion_ids = [None] * len(all_prompts)
+            completion_messages = [None] * len(all_prompts)
+            completion_mask = [None] * len(all_prompts)
+
+        completion_ids = broadcast_object_list(completion_ids, from_process=0)
+        completion_messages = broadcast_object_list(completion_messages, from_process=0)
+        completion_mask = broadcast_object_list(completion_mask, from_process=0)
+
+        process_slice = slice(
+            self.accelerator.process_index * len(prompts),
+            (self.accelerator.process_index + 1) * len(prompts),
+        )
+
+        completion_ids = completion_ids[process_slice]
+        completion_messages = completion_messages[process_slice]
+        completion_mask = completion_mask[process_slice]
+
+        # Pad + mask after per-sequence EOS tokens
+        completion_ids = [torch.tensor(ids, device=device) for ids in completion_ids]
+        completion_ids = pad(
+            completion_ids, padding_value=self.processing_class.pad_token_id
+        )  # type: ignore
+
+        completion_mask = [
+            torch.tensor(mask, device=device) for mask in completion_mask
+        ]
+        completion_mask = pad(completion_mask, padding_value=0)
+
+        prompt_completion_ids = torch.cat([prompt_ids, completion_ids], dim=1)
+        attention_mask = torch.cat([prompt_mask, completion_mask], dim=1)  # (B, P+C)
+
+        logits_to_keep = completion_ids.size(1)
+
+        with torch.no_grad():
+            # When using num_iterations == 1, old_per_token_logps == per_token_logps, so we can skip it's
+            # computation here, and use per_token_logps.detach() instead.
+            if self.num_iterations > 1:
+                old_per_token_logps = self._get_per_token_logps(
+                    self.model, prompt_completion_ids, attention_mask, logits_to_keep
+                )
+            else:
+                old_per_token_logps = None
+
+            if self.beta == 0.0:
+                ref_per_token_logps = None
+            elif self.ref_model is not None:
+                ref_per_token_logps = self._get_per_token_logps(
+                    self.ref_model,
+                    prompt_completion_ids,
+                    attention_mask,
+                    logits_to_keep,
+                )
+            else:
+                with self.accelerator.unwrap_model(self.model).disable_adapter():
+                    ref_per_token_logps = self._get_per_token_logps(
+                        self.model,
+                        prompt_completion_ids,
+                        attention_mask,
+                        logits_to_keep,
+                    )
+
+        # use message dicts for reward function inputs
+        completions = completion_messages
+        rewards_per_func = torch.zeros(
+            len(prompts), len(self.reward_funcs), device=device
+        )
+        for i, reward_func in enumerate(self.reward_funcs):
+            # Repeat all input columns (but "prompt" and "completion") to match the number of generations
+            keys = [key for key in inputs[0] if key not in ["prompt", "completion"]]  # type: ignore
+            reward_kwargs = {key: [example[key] for example in inputs] for key in keys}  # type: ignore
+            output_reward_func = reward_func(
+                prompts=prompts, completions=completions, **reward_kwargs
+            )  # type: ignore
+            rewards_per_func[:, i] = torch.tensor(
+                output_reward_func, dtype=torch.float32, device=device
+            )
+
+        rewards_per_func = gather(rewards_per_func)
+
+        # Apply weights to each reward function's output and sum
+        rewards = (rewards_per_func * self.reward_weights.to(device).unsqueeze(0)).sum(
+            dim=1
+        )
+
+        # Compute grouped-wise rewards
+        mean_grouped_rewards = rewards.view(-1, self.num_generations).mean(dim=1)  # type: ignore
+        std_grouped_rewards = rewards.view(-1, self.num_generations).std(dim=1)  # type: ignore
+
+        # Normalize the rewards to compute the advantages
+        mean_grouped_rewards = mean_grouped_rewards.repeat_interleave(
+            self.num_generations, dim=0
+        )  # type: ignore
+        std_grouped_rewards = std_grouped_rewards.repeat_interleave(
+            self.num_generations, dim=0
+        )  # type: ignore
+        advantages = (rewards - mean_grouped_rewards) / (std_grouped_rewards + 1e-4)
+
+        # Slice to keep only the local part of the data
+        process_slice = slice(
+            self.accelerator.process_index * len(prompts),
+            (self.accelerator.process_index + 1) * len(prompts),
+        )
+        advantages = advantages[process_slice]
+
+        # Log the metrics
+        mode = "eval" if self.control.should_evaluate else "train"
+
+        completion_length = (
+            self.accelerator.gather_for_metrics(completion_mask.sum(1))
+            .float()
+            .mean()
+            .item()
+        )  # type: ignore
+        self._metrics[mode]["completion_length"].append(completion_length)
+
+        reward_per_func = rewards_per_func.mean(0)  # type: ignore
+        for i, reward_func in enumerate(self.reward_funcs):
+            reward_func_name = reward_func.__name__  # type: ignore
+            self._metrics[mode][f"rewards/{reward_func_name}"].append(
+                reward_per_func[i].item()
+            )
+
+        self._metrics[mode]["reward"].append(rewards.mean().item())
+        self._metrics[mode]["reward_std"].append(std_grouped_rewards.mean().item())
+
+        if (
+            self.log_completions
+            and self.state.global_step % self.args.logging_steps == 0
+        ):
+            prompts_to_log = gather_object(prompts)
+            completions_to_log = gather_object(completions)
+            rewards_to_log = rewards.tolist()
+
+            if self.accelerator.is_main_process:
+                if is_rich_available():
+                    print_prompt_completions_sample(
+                        [str(prompts_to_log[0][-1]["content"])],
+                        [completions_to_log[0]],
+                        [rewards_to_log[0]],
+                        self.state.global_step,
+                    )
+                if (
+                    self.args.report_to
+                    and "wandb" in self.args.report_to
+                    and wandb.run is not None
+                ):  # type: ignore
+                    import pandas as pd
+
+                    # For logging
+                    table = {
+                        "step": [str(self.state.global_step)] * len(rewards),
+                        "prompt": prompts_to_log,
+                        "completion": completions_to_log,
+                        "reward": rewards.tolist(),
+                    }
+                    df = pd.DataFrame(table)
+                    wandb.log({"completions": wandb.Table(dataframe=df)})  # type: ignore
+
+        return {
+            "prompt_ids": prompt_ids,
+            "prompt_mask": prompt_mask,
+            "completion_ids": completion_ids,
+            "completion_mask": completion_mask,
+            "old_per_token_logps": old_per_token_logps,
+            "ref_per_token_logps": ref_per_token_logps,
+            "advantages": advantages,
+        }
