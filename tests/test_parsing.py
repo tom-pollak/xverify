2192f754c9a15d16bc432cad2dbad1a52d1474ad
Tom Pollak <tompollak1000@gmail.com>
2025-03-12 10:48:05 +0000

    testing

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,3 +1,4 @@
+# %%
 import xmltodict
 from vllm import LLM
 from pydantic import BaseModel
@@ -5,12 +6,20 @@ from pydantic import BaseModel
 from xverify import Env, ToolUse
 from xverify.tools import calculator, search
 
-if "llm" not in globals():  # interactive use
-    llm = LLM(model="Qwen/Qwen2.5-0.5B-Instruct", max_model_len=2000)
-
 
 class MultiToolUse(BaseModel):
     tool_use: list[ToolUse[calculator, search]]
+
+
+# %%
+
+if "llm" not in globals():  # interactive use
+    llm = LLM(
+        model="Qwen/Qwen2.5-1.5B-Instruct",
+        max_model_len=2000,
+        gpu_memory_utilization=0.5,
+        enforce_eager=True,
+    )
 
 
 env = Env(MultiToolUse)
@@ -23,7 +32,7 @@ print("*" * 80)
 
 outp = llm.generate(  # type: ignore
     f"Use 2 tools! first tool is calculator, calculate 2 + 2, second tool is search, search for the capital of France\n{env.doc}",
-    sampling_params=env.sampling_params(),
+    sampling_params=env.sampling_params(temperature=2.),
     use_tqdm=False,
 )
 text = outp[0].outputs[0].text
