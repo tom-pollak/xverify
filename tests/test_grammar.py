c538f94273651671b7eb65594170666056f7244f
Tom Pollak <tompollak1000@gmail.com>
2025-03-12 09:27:16 +0000

    pytest

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,15 +1,50 @@
-from pydantic import BaseModel
-from xverify import ToolUse
-from xverify.tools import calculator, search
+from typing import Annotated, Union, Literal
+from pydantic import BaseModel, Field
 from xverify.xml import generate_gbnf_grammar_and_documentation
 
 
-class Tools(BaseModel):
-    tool_use: ToolUse[calculator, search]
+class A(BaseModel):
+    type: Literal["a"]
+    a: int
 
 
-class MultiTool(BaseModel):
-    tool_use: list[ToolUse[calculator, search]]
+class B(BaseModel):
+    type: Literal["b"]
+    b: int
 
 
-grammar, doc = generate_gbnf_grammar_and_documentation([Tools])
+class Foo(BaseModel):
+    a: A
+
+
+class MultiFoo(BaseModel):
+    a: list[Foo]
+
+
+class MultiUnionFoo(BaseModel):
+    out: list[Annotated[Union[A, B], Field(discriminator="type")]]
+
+
+def test_simple_model():
+    grammar, doc = generate_gbnf_grammar_and_documentation([Foo])
+    assert grammar is not None
+    assert doc is not None
+    assert isinstance(grammar, str)
+    assert isinstance(doc, str)
+
+
+def test_model_with_list():
+    grammar, doc = generate_gbnf_grammar_and_documentation([MultiFoo])
+    assert grammar is not None
+    assert doc is not None
+    assert isinstance(grammar, str)
+    assert isinstance(doc, str)
+
+
+def test_model_with_union_list():
+    grammar, doc = generate_gbnf_grammar_and_documentation([MultiUnionFoo])
+    assert grammar is not None
+    assert doc is not None
+    assert isinstance(grammar, str)
+    assert isinstance(doc, str)
+
